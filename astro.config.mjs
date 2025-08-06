3bd7bafb83bcdc3cccfc3a1c4d03727151400f7c
Vicente Javier Viera Guízar <vicenteJVG@outlook.com>
2025-08-06 13:19:10 -0600

    Added main font and icons library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.scalaty.solutions",
   vite: {
     plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.fontsource(),
+      name: "Plus Jakarta Sans Variable",
+      cssVariable: "--font-pjs",
+      weights: ["100 900"]
+    }],
+    // csp: true
   }
 });
