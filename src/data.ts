90fa14d25506eac7d164a821aac153088f913ea0
Vicente Javier Viera Guízar <vicenteJVG@outlook.com>
2025-08-30 11:02:30 -0600

    Migrated all index.astro data to data.ts file. - FeatureHighlight.astro component main styles implemented. - Minor layout changes to Hero and Marquee components.

---
+++ src/data.ts
@@ -0,0 +1,96 @@
+import type { Brand } from "./components/Marquee.astro";
+import type { SoftwareSolution } from "./components/Card.astro";
+import type { Highlights } from "./components/FeatureHighlights.astro";
+import type { FeatureInfo } from "./components/FeatureCard.astro";
+
+export interface FeatureSection {
+	readonly highlights: Highlights;
+	readonly features: Array<FeatureInfo>;
+};
+
+export const brands: ReadonlyArray<Brand> = [
+	{ name: "IMPORTACIONES GUTIÉRREZ", logo: "/svg/logo1.svg" },
+	{ name: "Astro", logo: "/svg/logo1.svg" },
+	{ name: "Minecraft", logo: "/svg/logo1.svg" },
+	{ name: "GitHub", logo: "/svg/logo1.svg" },
+	{ name: "Mozilla", logo: "/svg/logo1.svg" },
+	{ name: "marimari_en", logo: "/svg/logo1.svg" }
+];
+
+export const softwareSolutions: ReadonlyArray<SoftwareSolution> = [
+	{
+		title: "Facturación electrónica",
+		description: "Este es un texto de prueba.",
+		icon: "Receipt"
+	},
+	{
+		title: "Inteligencia artificial empresarial",
+		description: "Este es un texto de prueba.",
+		icon: "Brain"
+	},
+	{
+		title: "Bases de datos y automatizaciones",
+		description: "Este es un texto de prueba.",
+		icon: "DatabaseZap"
+	}
+];
+
+export const featureSections: ReadonlyArray<FeatureSection> = [
+	{
+		highlights: {
+			feature: "Data Insights",
+			title: { first: "Improved", highlight: "Decision-Making" },
+			description: "By leveraging real-time insights and comprehensive data analysis, you can make informed decisions with confidence, reducing uncertainty."
+		},
+		features: [
+			{
+				title: "Comprehensive Data Visualization",
+				description: "With our advanced visualization tools, turn complex datasets into easy-to-understand charts, graphs, and dashboards",
+				icon: "HandMetal"
+			},
+			{
+				title: "Predictive Modeling",
+				description: "Leverage cutting-edge predictive analytics to forecast future trends and outcomes.",
+				icon: "HandMetal"
+			}
+		]
+	},
+	{
+		highlights: {
+			feature: "Automate Tasks",
+			title: { first: "Increased", highlight: " Operational Efficiency" },
+			description: "With a clear view of your data, you can identify inefficiencies and streamline operations."
+		},
+		features: [
+			{
+				title: "Automated Data Collection",
+				description: "Eliminate manual data entry and reduce human error with automated data collection.",
+				icon: "HandMetal"
+			},
+			{
+				title: "Workflow Optimization",
+				description: "Analyze and optimize your workflows by identifying bottlenecks and inefficiencies.",
+				icon: "HandMetal"
+			}
+		]
+	},
+	{
+		highlights: {
+			feature: "Customer Insights",
+			title: { first: "Enhanced", highlight: "Customer Satisfaction" },
+			description: "Use customer behavior data to tailor your products and services to meet user needs more effectively."
+		},
+		features: [
+			{
+				title: "Personalized Customer Insights",
+				description: "Gain a detailed view of individual customer behavior, preferences, and interactions.",
+				icon: "HandMetal"
+			},
+			{
+				title: "Sentiment Analysis",
+				description: "Use sentiment analysis to track and interpret customer feedback in real-time.",
+				icon: "HandMetal"
+			}
+		]
+	}
+];
